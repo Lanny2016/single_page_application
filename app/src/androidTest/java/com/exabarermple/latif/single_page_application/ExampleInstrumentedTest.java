fe07c5202d351c3fff85c741907998e2c227260a
latif <latif.hamid@cutesafe.com>
2019-05-22 10:19:18 +0300

    test comment

--- app/src/androidTest/java/com/exabarermple/latif/single_page_application/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/exabarermple/latif/single_page_application/ExampleInstrumentedTest.java
@@ -19,8 +19,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getTargetContext ();
 
-        assertEquals("com.exabarermple.latif.single_page_application", appContext.getPackageName());
+        assertEquals ( "com.exabarermple.latif.single_page_application", appContext.getPackageName () );
     }
 }
